c2e11540621ad43286cb96be646ec7ead657c5de
chiennd <chientran91@gmail.com>
2018-12-21 17:10:49 +0700

    - add Custom Click

--- coreandroid/src/main/java/core/chiennd/View/COnClickListener.java
+++ coreandroid/src/main/java/core/chiennd/View/COnClickListener.java
@@ -7,27 +7,18 @@ import core.chiennd.utility.Constants;
 
 public  abstract class COnClickListener implements View.OnClickListener {
 
-    private long lastClickTime;
+    private long mLastClickTime;
 
     public abstract void onSingleClick(View v);
 
     @Override
     public final void onClick(View v) {
-        long currentClickTime = SystemClock.uptimeMillis();
-        long elapsedTime = currentClickTime - lastClickTime;
-        lastClickTime = currentClickTime;
+        long currentClickTime = SystemClock.elapsedRealtime();
+        long elapsedTime = currentClickTime - mLastClickTime;
         if (elapsedTime <= Constants.Validate.MIN_CLICK_INTERVAL) {
             return;
         }
+        mLastClickTime = currentClickTime;
         onSingleClick(v);
-    }
-
-    public static View.OnClickListener wrap(final View.OnClickListener onClickListener) {
-        return new COnClickListener() {
-            @Override
-            public void onSingleClick(View v) {
-                onClickListener.onClick(v);
-            }
-        };
     }
 }
