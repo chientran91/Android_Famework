8018df103430f50c03df45d46eea443576ec9c9b
chiennd <chientran91@gmail.com>
2019-04-05 16:14:56 +0700

    - add implent

--- app/src/main/java/com/chiennd/demo/activity/MainActivity.java
+++ app/src/main/java/com/chiennd/demo/activity/MainActivity.java
@@ -7,8 +7,28 @@ import com.chiennd.demo.R;
 public class MainActivity extends BaseActivity {
 
     @Override
+    protected String getTag() {
+        return null;
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected int getLayoutResoure() {
+        return 0;
+    }
+
+    @Override
+    protected void bindDataToView() {
+
     }
 }
